c744d03453eb276770825718201ceda34c7dc5cb
gwiedeman <gregory.wiedeman1@gmail.com>
2018-11-20 13:59:08 -0500

    setup to run under /search subpath in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.prefix = '/search'
+  config.relative_url_root = '/search'
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  mount QuickSearch::Engine => "/"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope 'search' do
+    mount QuickSearch::Engine => "/"
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
