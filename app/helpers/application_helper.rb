4f2609b869a0e3be5d329658190174f0d5171ef4
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2020-12-31 15:19:59 -0500

    fixed helper issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
 	def source_name
       'Everything'
     end
+    def container_classes
+    	'container-fluid'
+    end
+    def main_content_classes
+      'col-lg-9'
+    end
+    def sidebar_classes
+      'page-sidebar col-lg-3'
+    end
 end
